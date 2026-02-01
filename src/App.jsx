8b8b6aea8aaf1d5e02fccb85bbeeff94e0958b30
Akil-Dikshan <akildikshan02@gmail.com>
2026-02-01 13:24:15 +0530

    refactor: remove redundant nested E-commerce-platform project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Button } from "./components/ui/button"
+import { Button } from "@/components/ui/button"
 import  Navigation from "./components/Navigation/Navigation"
-import { Switch } from "./components/ui/switch"
+import { Switch } from "@/components/ui/switch"
 const App = () => {
   return (
     <>
       <Navigation />
-      <Button />
+      <Button variant={"outline"}>Click me</Button>
+      <Switch/>
     </>
   )
 }
--- src/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = () => { 
-    return (
-        <button className="text-red-500 text-2xl">
-            click me daddy
-        </button>
-    )
-}
-
-export default Button;
